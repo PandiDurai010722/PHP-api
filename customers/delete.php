463ba65492beaa5c66a11361cb31b2924bf2ef40
PandiDurai010722 <pandidurai9009@gmail.com>
2023-08-10 06:15:14 +0530

    api

---
+++ customers/delete.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Access-Control-Allow-Orgin:*');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Method: DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
+
+include('function.php');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == "DELETE") {
+
+
+    $deletecustomer = deleteCustomer($_GET);
+    echo $deletecustomer;
+
+
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
